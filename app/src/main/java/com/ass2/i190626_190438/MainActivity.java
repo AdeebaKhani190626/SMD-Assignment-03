6d9cc749d99806922635ebb2eb9580fdfe0a3728
Adeeba Khan <i190626@nu.edu.pk>
2022-11-20 12:22:46 +0500

    Updated

--- app/src/main/java/com/ass2/i190626_190438/MainActivity.java
+++ app/src/main/java/com/ass2/i190626_190438/MainActivity.java
@@ -2,7 +2,9 @@ package com.ass2.i190626_190438;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, SignUp.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+            }
+        },5000);
     }
 }
